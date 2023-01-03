2bea8e155ca5aa9c073c8ae949b6c70ba4964853
NeiXuS <rdnneixus@gmail.com>
2023-01-03 21:50:37 +0100

    Decouvrir

---
+++ decouvrir.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Découvrir</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="decouvrir.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <header id="bg-img">
+        
+    </header>
+
+    <h3>L’eau accessible à tout le monde</h3>
+    <div class="text">
+       
+        <p>Crée en 2023, le site Eau de Paris est un service qui permet de recenser toutes les fontaines à boire d’Ile de France. <br>
+            L’objectif de ce service est de permettre aux plus grands nombres de personnes d’avoir accès gratuitement à de l’eau potable, par le biais d’une carte interactive qui regroupe l’ensemble de ces fontaines publiques.
+        </p>
+        <p>Eau de Paris en chiffres : <br>
+            <strong>1052 fontaines </strong>à Paris et en Ile-de-France <br>
+            <strong>483 000 m³</strong> d'eau potable distribué chaque jour <br>
+            <strong>120 litres d’eau</strong> consommé en moyenne par personne <br>
+            <strong>3 millions d’usagers </strong> <br>
+        </p>
+    </div>
+
+    <script src="header.js"></script>
+</body>
+
+</html>
